0fbdb981c9f89bd75b4d179c321bad0dedb011e5
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2024-09-13 19:48:31 +0200

    Push image after building

--- containers/build.sh
+++ containers/build.sh
@@ -3,7 +3,8 @@
 for os in "debian"; do
   for release in "bookworm"; do
     for arch in "amd64" "armhf" "arm64"; do
-      podman build -t repo.rcmd.space/debian-${release}:${arch} . -f ${os}/${release}/${arch}/Dockerfile
+      podman build -t repo.rcmd.space/debrewery-${release}:${arch} . -f ${os}/${release}/${arch}/Dockerfile
+      podman push -t repo.rcmd.space/debrewery-${release}:${arch}
     done
   done
 done
