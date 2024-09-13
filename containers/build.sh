ee0d2a209b0e6ceda0487f8eb2a389273a1a3094
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2024-09-13 19:51:56 +0200

    Removed incorrect flag

--- containers/build.sh
+++ containers/build.sh
@@ -4,7 +4,7 @@ for os in "debian"; do
   for release in "bookworm"; do
     for arch in "amd64" "armhf" "arm64"; do
       podman build -t repo.rcmd.space/debrewery-${release}:${arch} . -f ${os}/${release}/${arch}/Dockerfile
-      podman push -t repo.rcmd.space/debrewery-${release}:${arch}
+      podman push repo.rcmd.space/debrewery-${release}:${arch}
     done
   done
 done
