c99968172e61c082c8f231e0a72ed7b02fff6cf9
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2024-09-13 19:37:32 +0200

    Separated architecture strings

--- containers/build.sh
+++ containers/build.sh
@@ -2,7 +2,7 @@
 
 for os in "debian"; do
   for release in "bookworm"; do
-    for arch in "amd64 armhf arm64"; do
+    for arch in "amd64" "armhf" "arm64"; do
       podman build -t repo.rcmd.space/debian-${release}:${arch} . -f ${os}/${release}/${arch}/Dockerfile
     done
   done
