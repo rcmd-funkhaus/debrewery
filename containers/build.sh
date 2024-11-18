2fc0cc211ef07fc7b914dc564851ad57423985c9
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2024-11-18 11:56:30 +0100

    Added buildscript support

--- containers/build.sh
+++ containers/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 for os in "debian"; do
-  for release in "bookworm"; do
+  for release in "bookworm bullseye buster"; do
     for arch in "amd64" "armhf" "arm64"; do
       podman build -t repo.rcmd.space/debrewery-${release}:${arch} . -f ${os}/${release}/${arch}/Dockerfile
       podman push repo.rcmd.space/debrewery-${release}:${arch}
